2054fe715d6e02ccdd506b3aadb43a2c1be792dc
Giovanni Cappellotto <potomak84@gmail.com>
2021-11-07 14:37:45 -0500

    Wrap SVG arc in a class
    It's just a way to define a namespace for all functions defined in the
    JS referenced by that page.

--- build/svg-arc/src/index.js
+++ build/svg-arc/src/index.js
@@ -1,65 +1,73 @@
 "use strict";
-var svgArcRadius = 150;
-var svgArcOffset = [300, 200];
-var svgArcState = {
-    startAngle: 0,
-    sweep: 90,
-};
-function pathD() {
-    var start = pointInCircle(svgArcRadius, svgArcState.startAngle, svgArcOffset);
-    var end = pointInCircle(svgArcRadius, svgArcState.startAngle + svgArcState.sweep, svgArcOffset);
-    var move = "M " + svgArcOffset[0] + " " + svgArcOffset[1];
-    var line = "L " + start[0] + " " + start[1];
-    var arc = "A " + svgArcRadius + " " + svgArcRadius + " 0 " + (svgArcState.sweep > 180 ? 0 : 1) + " 0 " + end[0] + " " + end[1];
-    return move + " " + line + " " + arc + " Z";
-}
-function toRadians(angle) {
-    return (Math.PI / 180) * angle;
-}
-function pointInCircle(radius, angle, offset) {
-    var x = Math.cos(toRadians(angle)) * radius + offset[0];
-    var y = Math.sin(toRadians(angle)) * radius + offset[1];
-    return [x, y];
-}
-function updateSVG() {
-    var start = pointInCircle(svgArcRadius, svgArcState.startAngle, svgArcOffset);
-    var startDot = document.getElementById("start_dot");
-    startDot.setAttribute('cx', start[0].toString());
-    startDot.setAttribute('cy', start[1].toString());
-    var end = pointInCircle(svgArcRadius, svgArcState.startAngle + svgArcState.sweep, svgArcOffset);
-    var endDot = document.getElementById("end_dot");
-    endDot.setAttribute('cx', end[0].toString());
-    endDot.setAttribute('cy', end[1].toString());
-    var path = document.getElementById("path");
-    path.setAttribute('d', pathD());
-}
-function updateForm() {
-    var startAngleText = document.getElementById("start_angle_text");
-    startAngleText.textContent = svgArcState.startAngle.toString();
-    var sweepText = document.getElementById("sweep_text");
-    sweepText.textContent = svgArcState.sweep.toString();
-}
-function updateUI() {
-    updateSVG();
-    updateForm();
-}
-function onStartAngleChange(event) {
-    var value = event.target.value;
-    svgArcState.startAngle = parseInt(value, 10);
-    updateUI();
-}
-function onSweepChange(event) {
-    var value = event.target.value;
-    svgArcState.sweep = parseInt(value, 10);
-    updateUI();
-}
-window.onload = function () {
-    updateUI();
-    var startAngle = document.getElementById("start_angle");
-    startAngle.value = svgArcState.startAngle.toString();
-    startAngle.oninput = onStartAngleChange;
-    var sweep = document.getElementById("sweep");
-    sweep.value = svgArcState.sweep.toString();
-    sweep.oninput = onSweepChange;
-};
+var DSSVGArc = /** @class */ (function () {
+    function DSSVGArc() {
+    }
+    DSSVGArc.pathD = function () {
+        var start = this.pointInCircle(this.RADIUS, this.STATE.startAngle, this.OFFSET);
+        var end = this.pointInCircle(this.RADIUS, this.STATE.startAngle + this.STATE.sweep, this.OFFSET);
+        var move = "M " + this.OFFSET[0] + " " + this.OFFSET[1];
+        var line = "L " + start[0] + " " + start[1];
+        var arc = "A " + this.RADIUS + " " + this.RADIUS + " 0 " + (this.STATE.sweep > 180 ? 0 : 1) + " 0 " + end[0] + " " + end[1];
+        return move + " " + line + " " + arc + " Z";
+    };
+    DSSVGArc.toRadians = function (angle) {
+        return (Math.PI / 180) * angle;
+    };
+    DSSVGArc.pointInCircle = function (radius, angle, offset) {
+        var x = Math.cos(this.toRadians(angle)) * radius + offset[0];
+        var y = Math.sin(this.toRadians(angle)) * radius + offset[1];
+        return [x, y];
+    };
+    DSSVGArc.updateSVG = function () {
+        var start = this.pointInCircle(this.RADIUS, this.STATE.startAngle, this.OFFSET);
+        var startDot = document.getElementById("start_dot");
+        startDot.setAttribute('cx', start[0].toString());
+        startDot.setAttribute('cy', start[1].toString());
+        var end = this.pointInCircle(this.RADIUS, this.STATE.startAngle + this.STATE.sweep, this.OFFSET);
+        var endDot = document.getElementById("end_dot");
+        endDot.setAttribute('cx', end[0].toString());
+        endDot.setAttribute('cy', end[1].toString());
+        var path = document.getElementById("path");
+        path.setAttribute('d', this.pathD());
+    };
+    DSSVGArc.updateForm = function () {
+        var startAngleText = document.getElementById("start_angle_text");
+        startAngleText.textContent = this.STATE.startAngle.toString();
+        var sweepText = document.getElementById("sweep_text");
+        sweepText.textContent = this.STATE.sweep.toString();
+    };
+    DSSVGArc.updateUI = function () {
+        this.updateSVG();
+        this.updateForm();
+    };
+    DSSVGArc.onStartAngleChange = function (event) {
+        var value = event.target.value;
+        this.STATE.startAngle = parseInt(value, 10);
+        this.updateUI();
+    };
+    DSSVGArc.onSweepChange = function (event) {
+        var value = event.target.value;
+        this.STATE.sweep = parseInt(value, 10);
+        this.updateUI();
+    };
+    DSSVGArc.init = function () {
+        var _this = this;
+        console.log(this);
+        this.updateUI();
+        var startAngle = document.getElementById("start_angle");
+        startAngle.value = this.STATE.startAngle.toString();
+        startAngle.oninput = function (event) { return _this.onStartAngleChange(event); };
+        var sweep = document.getElementById("sweep");
+        sweep.value = this.STATE.sweep.toString();
+        sweep.oninput = function (event) { return _this.onSweepChange(event); };
+    };
+    DSSVGArc.RADIUS = 150;
+    DSSVGArc.OFFSET = [300, 200];
+    DSSVGArc.STATE = {
+        startAngle: 0,
+        sweep: 90,
+    };
+    return DSSVGArc;
+}());
+window.onload = function () { return DSSVGArc.init(); };
 //# sourceMappingURL=index.js.map
--- svg-arc/src/index.ts
+++ svg-arc/src/index.ts
@@ -1,77 +1,81 @@
-const svgArcRadius = 150
-const svgArcOffset: [number, number] = [300, 200]
+class DSSVGArc {
+  static RADIUS = 150
+  static OFFSET: [number, number] = [300, 200]
+  static STATE = {
+    startAngle: 0,
+    sweep: 90,
+  }
 
-const svgArcState = {
-  startAngle: 0,
-  sweep: 90,
+  static pathD(): string {
+    const start = this.pointInCircle(this.RADIUS, this.STATE.startAngle, this.OFFSET)
+    const end = this.pointInCircle(this.RADIUS, this.STATE.startAngle + this.STATE.sweep, this.OFFSET)
+    const move = `M ${this.OFFSET[0]} ${this.OFFSET[1]}`
+    const line = `L ${start[0]} ${start[1]}`
+    const arc = `A ${this.RADIUS} ${this.RADIUS} 0 ${this.STATE.sweep > 180 ? 0 : 1} 0 ${end[0]} ${end[1]}`
+    return `${move} ${line} ${arc} Z`
+  }
+
+  static toRadians(angle: number): number {
+    return (Math.PI / 180) * angle
+  }
+
+  static pointInCircle(radius: number, angle: number, offset: [number, number]): [number, number] {
+    const x = Math.cos(this.toRadians(angle)) * radius + offset[0]
+    const y = Math.sin(this.toRadians(angle)) * radius + offset[1]
+    return [x, y]
+  }
+
+  static updateSVG(): void {
+    const start = this.pointInCircle(this.RADIUS, this.STATE.startAngle, this.OFFSET)
+    const startDot = document.getElementById("start_dot") as unknown as SVGCircleElement
+    startDot.setAttribute('cx', start[0].toString())
+    startDot.setAttribute('cy', start[1].toString())
+
+    const end = this.pointInCircle(this.RADIUS, this.STATE.startAngle + this.STATE.sweep, this.OFFSET)
+    const endDot = document.getElementById("end_dot") as unknown as SVGCircleElement
+    endDot.setAttribute('cx', end[0].toString())
+    endDot.setAttribute('cy', end[1].toString())
+
+    const path = document.getElementById("path") as unknown as SVGPathElement
+    path.setAttribute('d', this.pathD())
+  }
+
+  static updateForm(): void {
+    const startAngleText = document.getElementById("start_angle_text") as HTMLSpanElement
+    startAngleText.textContent = this.STATE.startAngle.toString()
+    const sweepText = document.getElementById("sweep_text") as HTMLSpanElement
+    sweepText.textContent = this.STATE.sweep.toString()
+  }
+
+  static updateUI(): void {
+    this.updateSVG()
+    this.updateForm()
+  }
+
+  static onStartAngleChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value
+    this.STATE.startAngle = parseInt(value, 10)
+    this.updateUI()
+  }
+
+  static onSweepChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value
+    this.STATE.sweep = parseInt(value, 10)
+    this.updateUI()
+  }
+
+  static init(): void {
+    console.log(this)
+    this.updateUI()
+
+    const startAngle = document.getElementById("start_angle") as HTMLInputElement
+    startAngle.value = this.STATE.startAngle.toString()
+    startAngle.oninput = (event) => this.onStartAngleChange(event)
+
+    const sweep = document.getElementById("sweep") as HTMLInputElement
+    sweep.value = this.STATE.sweep.toString()
+    sweep.oninput = (event) => this.onSweepChange(event)
+  }
 }
 
-function pathD(): string {
-  const start = pointInCircle(svgArcRadius, svgArcState.startAngle, svgArcOffset)
-  const end = pointInCircle(svgArcRadius, svgArcState.startAngle + svgArcState.sweep, svgArcOffset)
-  const move = `M ${svgArcOffset[0]} ${svgArcOffset[1]}`
-  const line = `L ${start[0]} ${start[1]}`
-  const arc = `A ${svgArcRadius} ${svgArcRadius} 0 ${svgArcState.sweep > 180 ? 0 : 1} 0 ${end[0]} ${end[1]}`
-  return `${move} ${line} ${arc} Z`
-}
-
-function toRadians(angle: number): number {
-  return (Math.PI / 180) * angle
-}
-
-function pointInCircle(radius: number, angle: number, offset: [number, number]): [number, number] {
-  const x = Math.cos(toRadians(angle)) * radius + offset[0]
-  const y = Math.sin(toRadians(angle)) * radius + offset[1]
-  return [x, y]
-}
-
-function updateSVG(): void {
-  const start = pointInCircle(svgArcRadius, svgArcState.startAngle, svgArcOffset)
-  const startDot = document.getElementById("start_dot") as unknown as SVGCircleElement
-  startDot.setAttribute('cx', start[0].toString())
-  startDot.setAttribute('cy', start[1].toString())
-
-  const end = pointInCircle(svgArcRadius, svgArcState.startAngle + svgArcState.sweep, svgArcOffset)
-  const endDot = document.getElementById("end_dot") as unknown as SVGCircleElement
-  endDot.setAttribute('cx', end[0].toString())
-  endDot.setAttribute('cy', end[1].toString())
-
-  const path = document.getElementById("path") as unknown as SVGPathElement
-  path.setAttribute('d', pathD())
-}
-
-function updateForm(): void {
-  const startAngleText = document.getElementById("start_angle_text") as HTMLSpanElement
-  startAngleText.textContent = svgArcState.startAngle.toString()
-  const sweepText = document.getElementById("sweep_text") as HTMLSpanElement
-  sweepText.textContent = svgArcState.sweep.toString()
-}
-
-function updateUI(): void {
-  updateSVG()
-  updateForm()
-}
-
-function onStartAngleChange(event: Event): void {
-  const value = (event.target as HTMLInputElement).value
-  svgArcState.startAngle = parseInt(value, 10)
-  updateUI()
-}
-
-function onSweepChange(event: Event): void {
-  const value = (event.target as HTMLInputElement).value
-  svgArcState.sweep = parseInt(value, 10)
-  updateUI()
-}
-
-window.onload = function (): void {
-  updateUI()
-
-  const startAngle = document.getElementById("start_angle") as HTMLInputElement
-  startAngle.value = svgArcState.startAngle.toString()
-  startAngle.oninput = onStartAngleChange
-
-  const sweep = document.getElementById("sweep") as HTMLInputElement
-  sweep.value = svgArcState.sweep.toString()
-  sweep.oninput = onSweepChange
-}
+window.onload = () => DSSVGArc.init()
